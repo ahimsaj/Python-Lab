d6d04c106687a393ab6f665a4798b7ab218d4428
ahimsaj <90971644+ahimsaj@users.noreply.github.com>
2022-02-18 16:41:37 +0530

    Create Datatype.py

---
+++ Datatype.py
@@ -0,0 +1,36 @@
+# NUMBERIC TYPES
+# int
+a = 10
+print("a is of type ",type(a))
+
+#  float
+a = 7.2
+print("a is of type",type(a))
+
+#  complex
+a = 7+5j
+print("a is of type",type(a))
+
+# SEQUENCE TYPES
+#  list
+a = [1,2,3,4,5]
+print("a is of type",type(a))
+
+#  tuple
+a = (1,2,3,4,5)
+print("a is of type",type(a))
+
+# MAPPING TYPES
+# sets
+a = {1,2,3,4,5}
+print("a is of type",type(a))
+
+# TEXT TYPES
+# string
+a = 'Ahimsa Jain'
+print("a is of type",type(a))
+
+# BOOLEAN TYPES
+a = bool(5)
+print(a)
+print("a is of type",type(a))
