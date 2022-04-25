68ef02d9501582bd46487b3f5251901c6f4830c1
ahimsaj <90971644+ahimsaj@users.noreply.github.com>
2022-04-25 12:07:14 +0530

    Create Inheritance.py

---
+++ Inheritance.py
@@ -0,0 +1,21 @@
+class Person(object):
+    # Constructor
+def __init__(self, name):
+        self.name = name
+defgetName(self):
+        return self.name
+defisEmployee(self):
+        return False
+
+# Inherited or Subclass (Note Person in bracket)
+class Employee(Person):
+
+    # Here we return true
+defisEmployee(self):
+        return True
+if __name__=='__main__':
+emp = Person("Naruto") # An Object of Person
+    print(emp.getName(), emp.isEmployee())
+
+emp = Employee("Kakashi") # An Object of Employee
+    print(emp.getName(), emp.isEmployee())
